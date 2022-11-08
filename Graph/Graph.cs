dc728f180d73cc5b7d46f0ab35c12af120184a56
Rodrigo Espino <70110309+rodrigo-espino@users.noreply.github.com>
2022-11-07 19:31:44 -0600

    First Commit

---
+++ Graph/Graph.cs
@@ -0,0 +1,86 @@
+﻿using System;
+namespace Graph
+{
+    public class Graph
+    {
+        
+        public int[,] matrix;
+        public int size;
+
+        public Graph(int size)
+        {
+            this.size = size;
+            matrix = new int[size, size];
+        }
+        public void addEdge(int i, int j)
+        {
+            matrix[i, j] = 1;
+            matrix[j, i] = 1;
+        }
+        public void removeEdge(int i, int j)
+        {
+            matrix[i, j] = 0;
+            matrix[j, i] = 0;
+        }
+        public void print()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void printAdjacencyList()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(i + ": ");
+                for (int j = 0; j < size; j++)
+                {
+                    if (matrix[i, j] == 1)
+                    {
+                        Console.Write(j + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        public void printAdjacencyMatrix()
+        {
+            Console.Write("   ");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(i + " ");
+                if (i == size - 1)
+                {
+                    Console.WriteLine();
+                }
+            }
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(i + ": ");
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void printTraversing(int i, bool[] visited)
+        {
+            visited[i] = true;
+            Console.Write(i + " ");
+            for (int j = 0; j < size; j++)
+            {
+                if (matrix[i, j] == 1 && !visited[j])
+                {
+                    printTraversing(j, visited);
+                }
+            }
+        }
+    }
+}
+
---
+++ Graph/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Graph;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Graph graph = new Graph(4);
+
+        int i = 1;
+        int j = 2;
+        int k = 3;
+        int l = 4;
+        graph.addEdge(i, j);
+        graph.addEdge(j, k);
+        graph.addEdge(k, l);
+        graph.addEdge(j, k);
+        graph.addEdge(i, l);
+
+
+        graph.printAdjacencyList();
+        graph.printAdjacencyMatrix();
+
+
+        bool[] visited = new bool[4];
+        graph.printTraversing(1, visited);
+
+    }
+}
